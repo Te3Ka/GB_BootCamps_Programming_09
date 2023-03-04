87240f227ef17b05dcb7ba682ace92324a1fcffa
Te3Ka_PaynE <Mnement4813@yandex.ru>
2023-03-04 12:37:37 +0300

    2 страницы

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 from flask import Flask
 
 app = Flask(__name__)
+
+@app.route('/')
+def main():
+	return '<h1>Hello, World!</h1>'
+
+@app.route('/index')
+def index():
+	return "<h2>Это моя первая страница</h2>"
+
+if __name__ == '__main__':
+	app.run()

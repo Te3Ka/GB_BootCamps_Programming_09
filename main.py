5bb37f7d5b97247e49aa45de185852b1fc52acd8
Te3Ka_PaynE <Mnement4813@yandex.ru>
2023-03-05 14:22:39 +0300

    2 страницы с переходом, добавлены числа

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ app = Flask(__name__)
 def main():
 	return "<h1>Hello, World!</h1><a href='/index'>Перейти на 2-ую страницу</a>"
 
-@app.route('/index')
-def index():
-	return "<h2>Это моя первая страница</h2>"
+@app.route('/index/<x>/<y>')
+def index(x, y):
+	return f"<h2>Это моя первая страница</h2><br><h3>Реузльтат: {int(x) + int(y)}</h3>"
 
 if __name__ == '__main__':
 	app.run()
